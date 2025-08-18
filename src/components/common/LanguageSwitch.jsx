1f03269d8ac2aa100ebda5bd526a911ef36e1c49
marcxsdev <marcosalexandre1322@gmail.com>
2025-08-18 18:43:04 -0300

    feat(styles): add Raleway font

--- src/components/common/LanguageSwitch.jsx
+++ src/components/common/LanguageSwitch.jsx
@@ -12,7 +12,7 @@ const LanguageSwitch = () => {
       className="relative w-[121px] h-7 bg-white rounded-full shadow-[inset_0_6px_8px_3px_rgba(0,0,0,0.1)] flex items-center cursor-pointer overflow-hidden"
       onClick={toggleLanguage}
     >
-      <div className="flex w-full z-10 text-black font-medium text-sm">
+      <div className="flex w-full z-10 text-black font-raleway font-semibold text-sm">
         <div className="w-[60.5px] text-center">EN</div>
         <div className="w-[60.5px] text-center">PT</div>
       </div>
