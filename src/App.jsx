828de96d1c01d981f8ed125876f20b4ec8390d88
marcxsdev <marcosalexandre1322@gmail.com>
2025-08-31 13:50:43 -0300

    feat: add SkillCard component

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,11 @@ import "./App.css";
 
 import Header from "./components/sections/Header";
 import Introduction from "./components/sections/Introduction";
-import Skills from "./components/sections/Skills";
-
 function App() {
   return (
     <div>
       <Header />
       <Introduction />
-      <Skills />
     </div>
   );
 }
