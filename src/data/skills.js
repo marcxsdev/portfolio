9bf0deaf4666ab90748a703866ed42202f213a61
marcxsdev <marcosalexandre1322@gmail.com>
2025-08-31 13:49:34 -0300

    chore: add skills data

---
+++ src/data/skills.js
@@ -0,0 +1,13 @@
+import ReactIcon from "../assets/icons/react.svg";
+import JavaScriptIcon from "../assets/icons/javascript.svg";
+import NextjsIcon from "../assets/icons/nextjs.svg";
+import TailwindIcon from "../assets/icons/tailwind.svg";
+import NodejsIcon from "../assets/icons/nodejs.svg";
+
+export const skills = [
+  { name: "Next.js", icon: NextjsIcon },
+  { name: "React", icon: ReactIcon },
+  { name: "Javascript", icon: JavaScriptIcon },
+  { name: "TailwindCSS", icon: TailwindIcon },
+  { name: "Node.js", icon: NodejsIcon },
+];
