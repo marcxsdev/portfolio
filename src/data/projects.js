508e904b1a85239834afd97621e71b87e481a29a
marcxsdev <marcosalexandre1322@gmail.com>
2025-09-02 04:57:06 -0300

    feat(data): add initial dataset for ProjectCards

---
+++ src/data/projects.js
@@ -0,0 +1,16 @@
+export const projects = [
+  {
+    image: "/images/projects/myndsync-project.png",
+    title: "MindSync",
+    decriptionMobile: "Lorem",
+    descriptionFull: "lorem",
+    repolink: "https://github.com/marcxsdev/mindsync",
+  },
+  {
+    image: "/images/projects/reicell-project.png",
+    title: "Reicell Celular",
+    decriptionMobile: "Lorem",
+    descriptionFull: "lorem",
+    repolink: "https://github.com/marcxsdev/reicell-landing-page",
+  },
+];
