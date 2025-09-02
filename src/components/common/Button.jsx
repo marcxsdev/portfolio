8962d101f6ab57b54f81e69c8e0b7c144a0569e1
marcxsdev <marcosalexandre1322@gmail.com>
2025-09-02 04:30:40 -0300

    feat: add Button component

---
+++ src/components/common/Button.jsx
@@ -0,0 +1,16 @@
+const Button = ({ children, href, icon, className }) => {
+  return (
+    <a
+      href={href}
+      target="_blank"
+      className={`flex flex-row justify-center items-center gap-5 bg-[#6A4C9C] hover:bg-[#5C4189] transition-colors duration-200 ${className}`}
+    >
+      <span className="text-white font-raleway text-base font-semibold">
+        {children}
+      </span>
+      {icon && <span>{icon}</span>}
+    </a>
+  );
+};
+
+export default Button;
