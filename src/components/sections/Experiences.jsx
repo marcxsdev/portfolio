03800f96cb3a4b889671d6cf069763bfcd77faba
marcxsdev <marcosalexandre1322@gmail.com>
2025-09-03 09:53:53 -0300

    style(responsiveness): improve responsiveness of the Experiences component

--- src/components/sections/Experiences.jsx
+++ src/components/sections/Experiences.jsx
@@ -4,21 +4,23 @@ import ExperienceCard from "../ui/ExperienceCard";
 const Experiences = () => {
   return (
     <section className="py-10">
-      <h2 className="text-black font-raleway text-[52px] font-extrabold text-center py-5 md:text-6xl md:py-8">
-        Experiences
-      </h2>
+      <div className="mx-auto max-w-[1440px] px-[30px] md:px-[75px]">
+        <h2 className="text-black font-raleway text-[52px] font-extrabold text-center py-5 md:text-6xl md:py-8">
+          Experiences
+        </h2>
 
-      <div className="flex flex-col justify-center items-center gap-8 md:flex-row lg:flex-row">
-        {experiences.map((exp, index) => (
-          <ExperienceCard
-            key={index}
-            company={exp.company}
-            logo={exp.logo}
-            role={exp.role}
-            description={exp.description}
-            tags={exp.tags}
-          />
-        ))}
+        <div className="flex flex-col justify-center items-center gap-8 md:flex-row lg:flex-row">
+          {experiences.map((exp, index) => (
+            <ExperienceCard
+              key={index}
+              company={exp.company}
+              logo={exp.logo}
+              role={exp.role}
+              description={exp.description}
+              tags={exp.tags}
+            />
+          ))}
+        </div>
       </div>
     </section>
   );
