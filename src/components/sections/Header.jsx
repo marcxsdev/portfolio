dd739f9641444a1decd89a491ff791f9e907c735
marcxsdev <marcosalexandre1322@gmail.com>
2025-09-03 09:46:44 -0300

    style(responsiveness): improve responsiveness of the Header component

--- src/components/sections/Header.jsx
+++ src/components/sections/Header.jsx
@@ -1,18 +1,20 @@
+// Header.jsx
+
 import LanguageSwitch from "../common/LanguageSwitch";
 import ThemeSwitch from "../common/ThemeSwitch";
 import HamburguerMenu from "../common/HamburguerMenu";
 
 const Header = () => {
   return (
-    <header className="mx-[30px] md:mx-[75px] lg:max-w-[1440px] lg:mx-auto lg:w-full">
-      <div className="flex flex-row items-center justify-between py-9 md:py-12">
-        <LanguageSwitch />
-
-        <div className="flex flex-row items-center gap-12">
-          <ThemeSwitch />
-
-          <div className="hidden lg:flex">
-            <HamburguerMenu />
+    <header className="w-full">
+      <div className="mx-auto max-w-[1440px] px-[30px] md:px-[75px]">
+        <div className="flex flex-row items-center justify-between py-9 md:py-12">
+          <LanguageSwitch />
+          <div className="flex flex-row items-center gap-12">
+            <ThemeSwitch />
+            <div className="hidden lg:flex">
+              <HamburguerMenu />
+            </div>
           </div>
         </div>
       </div>
